2c3432333262c1847e80508f53c3ffa4eeae8294
Emmanuel <emmanuelm41@gmail.com>
2022-06-07 14:08:13 -0300

    remove dummy comment

--- assembly/helpers/ipld.ts
+++ assembly/helpers/ipld.ts
@@ -8,7 +8,6 @@ import {cast} from "../utils/cid/multihash";
 export function Put(mh_code: u64, mh_size: u32, codec: u64, data: Uint8Array): Cid {
     const id = create(codec, data)
 
-    // I really hate this CID interface. Why can't I just have bytes?
     const buf = new Uint8Array(MAX_CID_LEN)
     const cidLen = cid(id, mh_code, mh_size, buf)
 
