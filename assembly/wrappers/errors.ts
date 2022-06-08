67c18bb307b6ec284ec5cf0db74d1a457178c2c5
Emmanuel <emmanuelm41@gmail.com>
2022-06-08 08:44:48 -0300

    add new function to check caller on constructor

--- assembly/wrappers/errors.ts
+++ assembly/wrappers/errors.ts
@@ -12,6 +12,7 @@ export function usrForbidden(): isize{
 export function usrIllegalState(): isize{
     return genericAbort(USR_ILLEGAL_STATE, "unable to get method number")
 }
+
 export function genericAbort(code: u32, msg: string):isize{
     const buff = String.UTF8.encode(msg)
     const dataPtr = changetype<isize>(buff)
